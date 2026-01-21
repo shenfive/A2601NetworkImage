e2e18753b997c761c5f676db0a285836ebe8776d
Danny Shen <shenfive@gmail.com>
2026-01-21 14:52:06 +0800

    第一個網路下載

--- A2601NetworkImage/ViewController.swift
+++ A2601NetworkImage/ViewController.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var theImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        theImageView.backgroundColor = .yellow
+        
+        print(Date().timeIntervalSince1970)
+        let imageAddress = "https://cdn.hk01.com/di/media/images/dw/20220222/572848418750205952062874.jpeg/oo3mQK2GHHCbKZhbPh9fvYVNhYR__ec6pz3p1qc96dY?v=w640"
+        
+        if let imageURL = URL(string: imageAddress){
+            do{
+                let imageData = try Data(contentsOf: imageURL)
+                theImageView.image = UIImage(data: imageData)
+            }catch{
+                print(error.localizedDescription)
+            }
+        }
+        print(Date().timeIntervalSince1970)
+        
+        
+        
     }
 
 
