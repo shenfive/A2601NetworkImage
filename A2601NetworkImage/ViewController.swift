c7e28f0aeab2b2400c4cce541e1db90b88b2f150
Danny Shen <shenfive@gmail.com>
2026-01-21 15:55:37 +0800

    SDWebImage 載圖片＋快取

--- A2601NetworkImage/ViewController.swift
+++ A2601NetworkImage/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ViewController: UIViewController {
     @IBOutlet weak var theImageView: UIImageView!
@@ -17,31 +18,43 @@ class ViewController: UIViewController {
         theImageView.backgroundColor = .yellow
         
         print(Date().timeIntervalSince1970)
-        DispatchQueue.global().async {
+//        DispatchQueue.global().async {
             let imageAddress = "https://cdn.hk01.com/di/media/images/dw/20220222/572848418750205952062874.jpeg/oo3mQK2GHHCbKZhbPh9fvYVNhYR__ec6pz3p1qc96dY?v=w640"
+        theImageView.sd_setImage(with: URL(string: imageAddress))
             
-            if let imageURL = URL(string: imageAddress){
-                do{
-                    let imageData = try Data(contentsOf: imageURL)
-                    DispatchQueue.main.async {
-                        self.theImageView.image = UIImage(data: imageData)
-                        print("imageDidLoad:")
-                        print(Date().timeIntervalSince1970)
-                    }
-                }catch{
-                    print(error.localizedDescription)
-                }
-            }
-        }
+//            if let imageURL = URL(string: imageAddress){
+                
+                
+//                do{
+//                    let imageData = try Data(contentsOf: imageURL)
+//                    DispatchQueue.main.async {
+//                        self.theImageView.image = UIImage(data: imageData)
+//                        print("imageDidLoad:")
+//                        print(Date().timeIntervalSince1970)
+//                    }
+//                }catch{
+//                    print(error.localizedDescription)
+//                }
+//            }
+//        }
         
         
 
         print(Date().timeIntervalSince1970)
         
+  
         
         
     }
 
+    
+    override func viewWillAppear(_ animated: Bool) {
+        theImageView.clipsToBounds = false
+        theImageView.layer.shadowRadius = 20         //陰影
+        theImageView.layer.shadowOpacity = 0.6;
+        theImageView.layer.shadowColor = UIColor.gray.cgColor
+        theImageView.layer.shadowOffset = CGSize(width: 10, height: 10)
+    }
 
 }
 
