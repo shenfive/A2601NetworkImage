c3c8b0ef200ccb7120af3b9c1883118743d84823
Danny Shen <shenfive@gmail.com>
2026-01-22 10:17:57 +0800

    Fin

--- A2601NetworkImage/ViewController.swift
+++ A2601NetworkImage/ViewController.swift
@@ -13,6 +13,10 @@ import SwiftyJSON
 class ViewController: UIViewController {
     @IBOutlet weak var theImageView: UIImageView!
     
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,22 +27,35 @@ class ViewController: UIViewController {
 
     
     override func viewWillAppear(_ animated: Bool) {
-        
+        super.viewWillAppear(animated)
 
+        updateUser()
+    
+    }
+
+    @IBAction func nextAction(_ sender: Any) {
+        updateUser()
+    }
+    
+    func updateUser(){
         APIService.share.queryRandomUserAlamofire { [weak self] data, error in
             if let error{
                 //處理 error
                 print(error.localizedDescription)
                 return
             }
-            
+          
             if let data{
+                print(data)
+                let name = data["results"][0]["name"]["title"].stringValue + " " +
+                        data["results"][0]["name"]["first"].stringValue + " " +
+                        data["results"][0]["name"]["last"].stringValue
+                self?.nameLabel.text = name
+                
                 let imageAddress = data["results"][0]["picture"]["large"].stringValue
                 self?.theImageView.sd_setImage(with: URL(string: imageAddress))
             }
         }
-    
     }
-
 }
 
